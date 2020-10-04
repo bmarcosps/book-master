2b7cec1aec6ccc1af8f6739004c8664e0f7d47ba
bmarcosps <brunomarcos@ice.ufjf.br>
2020-10-04 11:25:37 -0300

    Change card styles to use Material UI

--- src/components/BookCard/index.tsx
+++ src/components/BookCard/index.tsx
@@ -1,29 +1,143 @@
+import { Card, CardActionArea, createStyles, makeStyles, Paper, Theme, Typography } from '@material-ui/core';
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Book from '../../models/Book';
 
 import './styles.css'
 
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      flexGrow: 1,
+      backgroundColor: theme.palette.grey[300],
+      minHeight: '100vh',
+      boxShadow: theme.shadows[6],
+    },
+    link: {
+        textDecoration: 'none',
+        color: 'inherit',
+        width: 400,
+        minWidth: 300,
+        maxHeight: 400,
+        height: 'auto',
+        marginBottom: 15,
+    },
+    card:{
+        overflow: 'hidden',
+        height:'100%',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'space-between',
+    },
+    cardAction: {
+        height:'100%',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'space-between',
+    },
+    bookInfo:{
+        width:'100%',
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'flex-start',
+        alignItems:'flex-start',
+        padding: '1rem',
+        textAlign: 'justify',
+    },
+    image: {
+        maxWidth: '10rem',
+        maxHeight: '10rem',
+        padding: '0 20px 0 20px'
+    },
+    bookStatusAvailable: {
+        width:'100%',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        minHeight: 50,
+        color: theme.palette.primary.contrastText,
+        backgroundColor: theme.palette.primary.light,
+    },
+    bookStatusTaken: {
+        width:'100%',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        minHeight: 50,
+        color: theme.palette.secondary.contrastText,
+        backgroundColor: theme.palette.secondary.light,
+    },
+    bookOwner: {
+        flex: 1,
+        width: '100%',
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-around'
+    }
+  }),
+);
 
 const BookCard: React.FC<Book> = (props) => {
+    const classes = useStyles();
+    const isTaken = false;
     return (
-        <Link className="book-item"  to={'/' + props.id.toString()}>
-                <main className="book-info">
-                    <img src={props.cover} alt="Teste"/>
-                    <div className="book-text">
-                        <strong>Título</strong><p>{props.title}</p>
-                        <strong>Autor</strong><p>{props.authors.map(a => a + "; ")}</p>
-                        <strong>Palavras-chave</strong><p>{props.keywords.map(a => a + "; ")}</p>
+        <Link className={classes.link}  to={'/' + props.id.toString()}>
+        <Card className={classes.card} elevation={3}>
+            <CardActionArea className={classes.cardAction}>
+        
+                <main className={classes.bookInfo}>
                     
+                    
+                    <img className={classes.image} src={props.cover} alt="Teste"/>
+                    <div className="book-text">
+                        <Typography variant="body1" color="textPrimary" component="p">
+                            Título
+                        </Typography>
+                        <Typography variant="body2" color="textSecondary" component="p" gutterBottom>
+                            {props.title}
+                        </Typography>
+
+                        <Typography variant="body1" color="textPrimary" component="p" >
+                            Autor
+                        </Typography>
+                        <Typography variant="body2" color="textSecondary" component="p" gutterBottom>
+                            {props.authors.map(a => a + "; ")}
+                        </Typography>
+
+                        <Typography variant="body1" color="textPrimary" component="p">
+                        Palavras-chave
+                        </Typography>
+                        <Typography variant="body2" color="textSecondary" component="p" gutterBottom>
+                        {props.keywords.map(a => a + "; ")}
+                        </Typography>
                     </div>
                     
                 </main>
-                <footer className="book-status available">
-                    
-                    <strong>Livro disponível para empréstimo</strong>
-                    
+                {isTaken?
+                    <footer className={classes.bookStatusTaken}>
+                        <Typography variant="body1"  component="p" gutterBottom>
+                            Livro emprestado
+                        </Typography>
+                        <div className={classes.bookOwner}>
+                        <Typography variant="body2" component="p" gutterBottom>
+                            Fulano de tal
+                        </Typography>
+                        <Typography variant="body2" component="p" gutterBottom>
+                            04/10/2020
+                        </Typography>
+                        </div>
+                    </footer>
+                :
+                <footer className={classes.bookStatusAvailable}>
+                    <Typography variant="body2" component="p">
+                        Livro disponível para empréstimo
+                    </Typography>
                 </footer>
-                
+                }
+            </CardActionArea>
+        </Card>
         </Link>
         
     )
--- src/pages/Book/index.tsx
+++ src/pages/Book/index.tsx
@@ -1,4 +1,4 @@
-import { createStyles, makeStyles, Theme } from '@material-ui/core';
+import { Button, createStyles, Grid, makeStyles, Paper, Theme } from '@material-ui/core';
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import Header from '../../components/Header';
@@ -13,20 +13,35 @@ const useStyles = makeStyles((theme: Theme) =>
     root: {
       flexGrow: 1,
     },
+    
+    paper: {
+      padding: theme.spacing(2),
+      textAlign: 'center',
+      color: theme.palette.text.secondary,
+    },
+    image: {
+      width:"100%"
+    }
   })
 );
 
 
 const Book = () => {
   const { id } = useParams<ParamTypes>();
-  const book = books[Number(id)];
+  const book = books.find(b => b.id === Number(id));
   const classes = useStyles();
-  return (
-    <div className={classes.root} >
-        <Header path={[book.title]}/>
-        <main className="book-info">
-            <img src={book.cover} alt="Teste"/>
-            <div className="book-text">
+
+  if(book) {
+    return (
+        <div className={classes.root} >
+          <Header path={[book.title]}/>
+
+          <Grid container spacing={3}>
+            <Grid item xs={4}>
+              <img className={classes.image} src={book.cover} alt="Teste"/>
+            </Grid>
+            <Grid className="book-text" item xs={8}>
+              
                 <strong>Título</strong><p>{book.title}</p>
                 <strong>Autor</strong><p>{book.authors.map(a => a + "; ")}</p>
                 <strong>Idioma</strong><p>{book.idiom}</p>
@@ -35,13 +50,25 @@ const Book = () => {
                 <strong>Páginas</strong><p>{book.pages}</p>
 
                 <strong>Palavras-chave</strong><p>{book.keywords.map(a => a + "; ")}</p>
+              
+            </Grid>
+            <Grid item xs={12}>
+              <Button variant="contained" color="primary" >Reservar</Button>
+              <Button variant="contained" color="secondary">Devolver</Button>
+            </Grid>
             
-        
-            </div>
-            
-        </main>
-      </div>
-  );
+          </Grid>
+        </div>
+      );
+  } else {
+    return (
+    <div>
+      <h3>
+        No match for <code>{id}</code>
+      </h3>
+    </div>
+    );
+  }
 }
 
 export default Book;
